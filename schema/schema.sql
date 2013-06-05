936b7e74159b65fc0f45cd8b622cc57e96cd31f8
Ezequiel Rivadeneira Lichardi <ezekiel@ezekiel.(none)>
2013-06-05 18:59:32 -0300

    Comienzo del proyecto. Schema de la bd

---
+++ schema/schema.sql
@@ -0,0 +1,89 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+CREATE SCHEMA IF NOT EXISTS `SysGym` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `SysGym` ;
+
+-- -----------------------------------------------------
+-- Table `SysGym`.`Empleado`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SysGym`.`Empleado` ;
+
+CREATE  TABLE IF NOT EXISTS `SysGym`.`Empleado` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `usuario` VARCHAR(45) NOT NULL ,
+  `password` BLOB NOT NULL ,
+  `nombre` VARCHAR(50) NULL ,
+  `direccion` VARCHAR(100) NULL ,
+  `telefono_particular` VARCHAR(20) NULL ,
+  `telefono_movil` VARCHAR(20) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SysGym`.`General`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SysGym`.`General` ;
+
+CREATE  TABLE IF NOT EXISTS `SysGym`.`General` (
+  `variable` VARCHAR(20) NOT NULL ,
+  `valor` VARCHAR(100) NOT NULL ,
+  PRIMARY KEY (`variable`) ,
+  UNIQUE INDEX `variable_UNIQUE` (`variable` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SysGym`.`Estado`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SysGym`.`Estado` ;
+
+CREATE  TABLE IF NOT EXISTS `SysGym`.`Estado` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(20) NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SysGym`.`Maquina`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SysGym`.`Maquina` ;
+
+CREATE  TABLE IF NOT EXISTS `SysGym`.`Maquina` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `descripcion` VARCHAR(100) NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
+  INDEX `fk_Maquina_Estado` (`id` ASC) ,
+  CONSTRAINT `fk_Maquina_Estado`
+    FOREIGN KEY (`id` )
+    REFERENCES `SysGym`.`Estado` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SysGym`.`Socio`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SysGym`.`Socio` ;
+
+CREATE  TABLE IF NOT EXISTS `SysGym`.`Socio` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(90) NOT NULL ,
+  `direccion` VARCHAR(45) NULL ,
+  `telefono_movil` VARCHAR(45) NULL ,
+  `telefono_particular` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `idSocio_UNIQUE` (`id` ASC) )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
