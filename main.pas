1c8ee9b0b4e9d9af69e11a624d8beb06e6ef0cdb
ezekielriva <ezerivadeneiral@gmail.com>
2013-06-05 22:12:29 -0300

    Menu principal

--- main.pas
+++ main.pas
@@ -5,14 +5,34 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, login, conexiones;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus, login,
+  conexiones;
 
 type
 
   { Tform_main }
 
   Tform_main = class(TForm)
+    menu_principal: TMainMenu;
+    m_inventario: TMenuItem;
+    m_reportes: TMenuItem;
+    m_reporte_mensual: TMenuItem;
+    m_maquinaria: TMenuItem;
+    m_categoria: TMenuItem;
+    m_gestion_socios: TMenuItem;
+    m_checkins: TMenuItem;
+    m_pago_cuotas: TMenuItem;
+    m_socios: TMenuItem;
+    m_empleados: TMenuItem;
+    m_preferencias: TMenuItem;
+    m_ayuda: TMenuItem;
+    m_sobre: TMenuItem;
+    m_salir: TMenuItem;
+    m_configuracion: TMenuItem;
+    m_gestion_empleados: TMenuItem;
     procedure FormCreate(Sender: TObject);
+    procedure m_inventarioClick(Sender: TObject);
+    procedure m_salirClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -32,6 +52,16 @@ implementation
 procedure Tform_main.FormCreate(Sender: TObject);
 begin
      MessageDlg('Bienvenido', 'Bienvenido al Sistema de Gestión de Gimnasios', mtInformation, [mbClose], 0);
+end;
+
+procedure Tform_main.m_inventarioClick(Sender: TObject);
+begin
+
+end;
+
+procedure Tform_main.m_salirClick(Sender: TObject);
+begin
+
 end;
 
 end.
