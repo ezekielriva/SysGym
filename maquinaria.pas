22754ce4dfe9e84f81b8ab58c38ed465f3d3076d
ezekielriva <ezerivadeneiral@gmail.com>
2013-06-09 18:47:26 -0300

    Quitado los filtros

--- maquinaria.pas
+++ maquinaria.pas
@@ -6,27 +6,21 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, DBGrids,
-  StdCtrls, DbCtrls, ZDataset, ZSqlUpdate, conexiones, db;
+  StdCtrls, DbCtrls, ZDataset, conexiones, db;
 
 type
 
   { Tform_maquina }
 
   Tform_maquina = class(TForm)
-    btn_limpiar_filtro: TButton;
     btn_salir: TButton;
     btn_nuevo: TButton;
     btn_borrar: TButton;
     btn_aplicar: TButton;
-    ds_categoria: TDatasource;
-    ds_estado: TDatasource;
-    dbcb_filtro_estado: TDBComboBox;
-    dbcb_filtro_categoria: TDBComboBox;
+    btn_mostrar: TButton;
     ds_Maquina: TDatasource;
     dbGrid_maquina: TDBGrid;
-    gb_filtro: TGroupBox;
     gb_acciones: TGroupBox;
-    tLog: TMemo;
     ZROQuery_Categoria: TZReadOnlyQuery;
     ZROQuery_Categoriaid: TLongintField;
     ZROQuery_Categorianombre: TStringField;
@@ -42,13 +36,10 @@ type
     zTable_id: TLongintField;
     procedure btn_aplicarClick(Sender: TObject);
     procedure btn_borrarClick(Sender: TObject);
-    procedure btn_limpiar_filtroClick(Sender: TObject);
+    procedure btn_mostrarClick(Sender: TObject);
     procedure btn_nuevoClick(Sender: TObject);
     procedure btn_salirClick(Sender: TObject);
-    procedure dbcb_filtro_estadoChange(Sender: TObject);
-    procedure dbGrid_maquinaCellClick(Column: TColumn);
     procedure FormCreate(Sender: TObject);
-    procedure LongintField1Change(Sender: TField);
   private
     { private declarations }
   public
@@ -71,12 +62,6 @@ begin
   zTableMaquina.Open;
 end;
 
-procedure Tform_maquina.LongintField1Change(Sender: TField);
-begin
-
-end;
-
-
 procedure Tform_maquina.btn_nuevoClick(Sender: TObject);
 begin
   zTableMaquina.Insert;
@@ -90,34 +75,26 @@ begin
   Close;
 end;
 
-procedure Tform_maquina.dbcb_filtro_estadoChange(Sender: TObject);
-begin
-  if dbcb_filtro_estado.ItemIndex > 0 then
-  begin
-       zTableMaquina.Filter:= Concat('Estado_id =', IntToStr(dbcb_filtro_estado.ItemIndex));
-       zTableMaquina.Filtered:=True;
-  end;
-end;
-
-procedure Tform_maquina.dbGrid_maquinaCellClick(Column: TColumn);
-begin
-
-end;
-
 procedure Tform_maquina.btn_borrarClick(Sender: TObject);
 begin
   zTableMaquina.Delete;
+end;
+
+procedure Tform_maquina.btn_mostrarClick(Sender: TObject);
+begin
+  ZROQuery_Categoria.Close;
+  ZROQuery_Estado.Close;
+  zTableMaquina.Close;
+
+  ZROQuery_Categoria.Open;
+  ZROQuery_Estado.Open;
+  zTableMaquina.Open;
 end;
 
 procedure Tform_maquina.btn_aplicarClick(Sender: TObject);
 begin
      if zTableMaquina.Modified then
         zTableMaquina.ApplyUpdates;
-end;
-
-procedure Tform_maquina.btn_limpiar_filtroClick(Sender: TObject);
-begin
-     zTableMaquina.Filtered:=False;
 end;
 
 end.
